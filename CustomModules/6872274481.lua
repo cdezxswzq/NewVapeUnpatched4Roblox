f1a4e860bb2679c152aeaba4ca53e79b95269319
cdezxswzq <128815560+cdezxswzq@users.noreply.github.com>
2023-12-26 15:03:24 -0500

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -12841,11 +12841,11 @@ runFunction(function()
 	local AnticheatBypassShowRoot = {Enabled = false}
 
 	local BypassNumbers = {
-		Lerp = 0.3,
-		Frequency = 0.33,
+		Lerp = 0.4,
+		Frequency = 0.43,
 		TPLowest = 0.05,
-		TPPrecise = 20,
-		TPRecheck = 20,
+		TPPrecise = 23,
+		TPRecheck = 23,
 		SlowdownDistance = 300,
 		Slowdown = 0.7,
 	}
